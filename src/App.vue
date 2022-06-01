c285700bc59369590bdd2ed011550038042d4e40
vijaykumar <vijay1111mane@gmail.com>
2022-06-01 17:34:11 +0530

    commit;

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
 
+<router-view />
 
-<SignUp />
 </template>
 
 <script>
-import SignUp from './components/SignUp.vue'
+
 export default {
   name: 'App',
-  components: {
-    SignUp
-  },
-  methods:{
-    
-  }
-
 }
 </script>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>Hello Welcome to Home Page</h1>
+</template>
+
+<script>
+export default{
+    name: "Home-Page ",
+}
+</script>
